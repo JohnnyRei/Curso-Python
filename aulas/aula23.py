7413fd2b05a74b7906d23ce6a5331db0c0d099d8
Johnny Guimarães <johnnyreisudi@gmail.com>
2022-08-08 16:57:54 -0300

    v7

---
+++ aulas/aula23.py
@@ -0,0 +1,17 @@
+"""
+* Listas em Python
+* Fatiamento
+* append, insert, pop, del, clear, extend, min e max.
+* range
+"""
+
+# texto = ("valor") # Texto é uma variável que recebe uma string
+# lista = [1, 2, 3, 4, 5, "Johnny", True, 10.5] # Lista é uma variável que recebe uma lista
+
+#         0    1    2    3    4
+lista = ["A", "B", "C", "D", "E"]
+# -       5    4    3    2    1
+
+string = 'ABCDE'
+
+print(lista[1])
